6e7e522436c0d4acfd9290a4a62168ce04b6050c
UththaraMadubhashini <uththaramadubhashini99@gmail.com>
2024-11-19 12:00:33 +0530

    manage files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Button variant="contained">Contained</Button>
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
