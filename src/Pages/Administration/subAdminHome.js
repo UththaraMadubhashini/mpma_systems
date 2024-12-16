2a2445d4fd24ef80160bde1542dcd96b8dad67a6
UththaraMadubhashini <uththaramadubhashini99@gmail.com>
2024-12-16 12:03:43 +0530

    create administration management

---
+++ src/Pages/Administration/subAdminHome.js
@@ -0,0 +1,130 @@
+import React from "react";
+import {
+  Container,
+  Typography,
+  Button,
+  Box,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import { Link } from "react-router-dom";
+import SideBarAdmin from "../../Components/SlideBar/sideBarAdmin";
+
+
+function Administration() {
+  return (
+    <div>
+        <SideBarAdmin />
+      {/* <Header /> */}
+      <Container maxWidth="lg" sx={{ marginTop: "20px", marginLeft:"330px" }}>
+        {/* Page Title */}
+        <Typography variant="h5" color="primary" gutterBottom sx={{marginLeft:"-20px"}}>
+          Administration Management
+        </Typography>
+        <Typography variant="body2" color="textSecondary" gutterBottom sx={{marginLeft:"-20px"}}>
+          Home / Administration Management
+        </Typography>
+
+        {/* Button Section */}
+        <Box 
+          
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            marginTop: "20px",
+            marginLeft:"-20px"
+          }}
+        >
+          {/* Course Registration Button */}
+          <Button
+            component={Link}
+            to="/course-registration"
+            variant="outlined"
+            startIcon={<EventNoteIcon />}
+            sx={{
+              width: "200px",
+              height: "150px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "10px",
+              fontSize: "16px",
+              fontWeight: "bold",
+              textTransform: "none",
+            }}
+          >
+            Course Registration
+          </Button>
+
+          {/* Batch Registration Button */}
+          <Button
+            component={Link}
+            to="/batch-registration"
+            variant="outlined"
+            startIcon={<AssignmentIcon />}
+            sx={{
+              width: "200px",
+              height: "150px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "10px",
+              fontSize: "16px",
+              fontWeight: "bold",
+              textTransform: "none",
+            }}
+          >
+            Batch Registration
+          </Button>
+
+          {/* Cost-In Button */}
+          <Button
+            variant="outlined"
+            startIcon={<AttachMoneyIcon />}
+            sx={{
+              width: "200px",
+              height: "150px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "10px",
+              fontSize: "16px",
+              fontWeight: "bold",
+              textTransform: "none",
+            }}
+          >
+            Cost - In
+          </Button>
+
+          {/* Add New Button */}
+          <Button
+            variant="outlined"
+            startIcon={<AddIcon />}
+            sx={{
+              width: "200px",
+              height: "150px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "10px",
+              fontSize: "16px",
+              fontWeight: "bold",
+              textTransform: "none",
+            }}
+          >
+            Add New
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
+}
+
+export default Administration;
--- src/Routes/route.js
+++ src/Routes/route.js
@@ -25,6 +25,8 @@ import Course from "../Pages/Course_Batch/subCourseHome"
 import Dashboard from "../Pages/Dashboard";
 import CourseRegistration from "../Pages/Course_Batch/courseRegistration";
 import BatchRegistration from "../Pages/Course_Batch/batchRegistration";
+import InstituteSetup from "../Pages/Administration/institute_setup";
+import Administration from "../Pages/Administration/subAdminHome";
 
 const Routers = () => {
   return (
@@ -34,6 +36,12 @@ const Routers = () => {
         <Route path="/course" element={<Course />} />
         <Route path="/course-registration" element={<CourseRegistration />} />
         <Route path="/batch-registration" element={<BatchRegistration />} />
+        
+      </Route>
+
+      <Route>
+        <Route path="/institute-setup" element={<InstituteSetup />} />
+        <Route path="/administration" element={<Administration />} />
       </Route>
     </Routes>
   );
